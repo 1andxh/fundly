52ef1b46fc8dcf1fee5f95a07174a3234f917a47
andoh <solomonkingsman@gmail.com>
2026-03-02 20:48:48 +0000

    Add base auth exceptions

---
+++ src/auth/exceptions.py
@@ -0,0 +1,10 @@
+from fastapi import status
+
+
+class FundlyException(Exception):
+    """Base class for all exceptions"""
+
+    def __init__(self, message: str, status_code: int = status.HTTP_400_BAD_REQUEST):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(self.message)
