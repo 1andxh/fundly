f6c4ce5bad2b809d961341b7537701f2d995ac33
andoh <solomonkingsman@gmail.com>
2026-03-12 22:50:40 +0000

    Implement verification mail send on account creation

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, status, BackgroundTasks
+from fastapi import APIRouter, Depends, status, BackgroundTasks, Response
+from fastapi.responses import JSONResponse
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.main import get_session
@@ -7,11 +8,12 @@ from .models import User
 from .schemas import Token, UserCreateModel, UserLoginModel, UserResponseModel
 from .services import UserService
 from src.mail.schemas import EmailValidator
-from src.mail.services import MailService
 from src.mail.utils import decode_url_safe_token
 from src.mail.mail import create_message, mail
 from datetime import datetime as dt
 from src.templates.templates import templates
+from src.mail.services import MailService
+from src.mail.utils import send_verification_mail
 
 auth_router = APIRouter()
 user_service = UserService()
@@ -45,9 +47,22 @@ async def send_mail(emails: EmailValidator, bg_task: BackgroundTasks):
     status_code=status.HTTP_201_CREATED,
 )
 async def signup(
-    user_data: UserCreateModel, session: AsyncSession = Depends(get_session)
+    bg_task: BackgroundTasks,
+    user_data: UserCreateModel,
+    session: AsyncSession = Depends(get_session),
 ):
-    return await user_service.create_user(user_data, session)
+    new_user = await user_service.create_user(user_data, session)
+    await send_verification_mail(new_user, bg_task)
+    return JSONResponse(
+        content={
+            "message": "A link to verify your account has been sent to your email"
+        },
+        status_code=200,
+    )
+
+
+@auth_router.get("/verify")
+async def verify_user(): ...
 
 
 @auth_router.post("/login", response_model=Token)
